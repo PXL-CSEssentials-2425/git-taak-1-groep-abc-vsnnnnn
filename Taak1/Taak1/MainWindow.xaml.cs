414ff7936dd70494edd36de1637bf1633cfcc274
vsnnnnn <christian.bomboi@student.pxl.be>
2024-10-07 22:09:10 +0200

    fix commit KLEUR-05

--- Taak1/Taak1/MainWindow.xaml.cs
+++ Taak1/Taak1/MainWindow.xaml.cs
@@ -34,21 +34,27 @@ namespace Taak1
         private void klikOpGeel(object sender, MouseButtonEventArgs e)
         {
             info.Text = "#FFFF00";
+            informatie.Text = "Geel is de kleur van levenslust";
+
         }
 
         private void klikOpBlauw(object sender, MouseButtonEventArgs e)
         {
             info.Text = "#0000FF";
+            informatie.Text = "Blauw is de kleur van intelligentie";
+
         }
 
         private void klikOpRood(object sender, MouseButtonEventArgs e)
         {
             info.Text = "#FF0000";
+            informatie.Text = "Rood is de kleur van warmte";
         }
 
         private void klikOpGroen(object sender, MouseButtonEventArgs e)
         {
             info.Text = "#008000";
+            informatie.Text = "Groen is de kleur van genezing";
         }
     }
 }
